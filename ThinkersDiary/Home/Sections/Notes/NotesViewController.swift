76e580f39949b4a73689688d3cc683ac8ff61f09
Jyothish Johnson <jyothish.johnson123@gmail.com>
2020-11-14 13:07:06 +0530

    added alert extension

--- ThinkersDiary/Home/Sections/Notes/NotesViewController.swift
+++ ThinkersDiary/Home/Sections/Notes/NotesViewController.swift
@@ -8,16 +8,32 @@
 import UIKit
 
 class NotesViewController: UIViewController {
-
+    
     @IBOutlet weak var name: UILabel!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        setUpViews()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         
         name.text = title
+    }
+    
+    func setUpViews(){
+        
+    }
+    
+    @IBAction func addFolderButtonAction(_ sender: UIButton) {
+        
+        let alert = UIAlertController.promptForFolderName { folderName  in
+            if let name = folderName {
+                print(name)
+            }
+        }
+        
+        present(alert , animated: true)
     }
 }
---
+++ ThinkersDiary/Utils/Extensions/UIAlertController.swift
@@ -0,0 +1,30 @@
+//
+//  UIAlertController.swift
+//  ThinkersDiary
+//
+//  Created by jyothish.johnson on 14/11/20.
+//
+
+import UIKit
+
+extension UIAlertController {
+    
+    static func promptForFolderName(_ completion : @escaping (String?) -> ()) -> UIAlertController{
+        
+        let ac = UIAlertController(title: "Enter folder name", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned ac] _ in
+            let folderTF = ac.textFields![0]
+            let folderName = folderTF.text
+            if folderName != nil && !(folderName?.isEmpty ?? true) {
+                completion(folderName)
+            }
+        }
+        
+        ac.addAction(submitAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        return ac
+    }
+}
