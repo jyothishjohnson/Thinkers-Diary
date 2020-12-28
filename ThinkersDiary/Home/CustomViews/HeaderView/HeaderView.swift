e77d0c1374da3f40c84f29f1f33ecf030980b940
Jyothish Johnson <jyothish.johnson123@gmail.com>
2020-12-28 18:08:44 +0530

    added headerview template

---
+++ ThinkersDiary/Home/CustomViews/HeaderView/HeaderView.swift
@@ -0,0 +1,19 @@
+//
+//  HeaderView.swift
+//  ThinkersDiary
+//
+//  Created by jyothish.johnson on 28/12/20.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+    
+    @IBOutlet weak var contentView: UIView!
+    
+
+
+    @IBAction func rightButtonAction(_ sender: UIButton) {
+        print(#function)
+    }
+}
