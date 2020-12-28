9cf1eddabffd706457bbe6587d6ee5dbe3decf1f
Jyothish Johnson <jyothish.johnson123@gmail.com>
2020-12-28 18:52:46 +0530

    added custom header to folderlist, noteslist VC's

--- ThinkersDiary/Home/CustomViews/HeaderView/HeaderView.swift
+++ ThinkersDiary/Home/CustomViews/HeaderView/HeaderView.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class HeaderView: UIView {
     
+    @IBOutlet var rootView: UIView!
     @IBOutlet weak var contentView: UIView!
     
-
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpView()
+    }
 
     @IBAction func rightButtonAction(_ sender: UIButton) {
         print(#function)
+    }
+    
+    func setUpView(){
+        Bundle.main.loadNibNamed("HeaderView", owner: self, options: nil)
+        addSubview(rootView)
+        rootView.frame = self.bounds
+        rootView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     }
 }
